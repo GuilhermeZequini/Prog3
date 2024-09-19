3159b5a6f8fcb01934fec2e0c298352269781826
Guilherme Zequini <guilherzb02@gmail.com>
2024-09-19 17:18:35 -0300

    oi

---
+++ aula01/Animal.java
@@ -0,0 +1,25 @@
+class Animal {
+	private String especie;
+	private String sexo;
+	private float peso;
+	private int idade;
+	private int id;
+
+	public Animal(String especie){
+		// this é a alocação de memória 
+		this.especie = especie;
+		idade = 0;
+	}
+
+	public void comer (){}
+
+	public float defecar(){
+		return 0.0f;
+	}
+	public void exibirInformacoes(){
+		System.out.println("Esse animal e uma" + especie);
+		System.out.println("A idade dele e " + idade);
+		System.out.println("E esse e " + this );
+		System.out.println();
+	}
+}
---
+++ aula01/Teste.java
@@ -0,0 +1,12 @@
+class Teste {
+	public static void main (String [] args){
+		Animal animal1 = new Animal("Felino");
+		Animal animal2 = new Animal("Suino");
+		Animal animal3 = new Animal("Equino");
+		Animal animal4 = new Animal("Bovino");
+
+		animal1.exibirInformacoes();
+		animal2.exibirInformacoes();
+		animal3.exibirInformacoes();
+	}
+}
