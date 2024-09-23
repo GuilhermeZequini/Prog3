1ee421e33b49dcda1ec7b453005f36915bdb12a3
Guilherme <guilherzb02@gmail.com>
2024-09-23 11:36:13 -0300

    exercicio

---
+++ exercicio1/Cliente.java
@@ -0,0 +1,14 @@
+class Cliente {
+    private int NumeroConta
+    private String Agencia
+    private float Saldo
+    private float Limite
+
+    public Cliente (int NumeroConta ,String Agencia ,float Limite){
+        this.NumeroConta = NumeroConta
+        this.Agencia = Agencia
+    }
+    public 
+     
+
+}
