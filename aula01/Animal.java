31027a2b726ec65ea8efa6ef31f1bba11679680e
Guilherme <guilherzb02@gmail.com>
2024-09-23 11:00:53 -0300

    teste

--- aula01/Animal.java
+++ aula01/Animal.java
@@ -17,7 +17,7 @@ class Animal {
 		return 0.0f;
 	}
 	public void exibirInformacoes(){
-		System.out.println("Esse animal e uma" + especie);
+		System.out.println("Esse animal e uma " + especie);
 		System.out.println("A idade dele e " + idade);
 		System.out.println("E esse e " + this );
 		System.out.println();
