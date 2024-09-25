d44918b126fe29cf2b92477aaff306ea3754bdff
Guilherme <guilherzb02@gmail.com>
2024-09-24 23:28:58 -0300

    exercicio

--- exercicio1/PessoaFisica.java
+++ exercicio1/PessoaFisica.java
@@ -1,6 +1,19 @@
 public class PessoaFisica extends Cliente { // Usando extends para herdar de Classe1
 
-    public PessoaFisica (int NumeroConta ,String Agencia ,float Limite){
+    private int CPF;
+    private String Nome;
+
+    public PessoaFisica (int NumeroConta ,String Agencia ,int CPF , String Nome){
         super(NumeroConta ,Agencia ,Limite);
     }
+
+    public int getCFP(){
+        return CPF;
+    }
+
+    public String getNome(){
+        return Nome;
+    }
+
+    
 }
