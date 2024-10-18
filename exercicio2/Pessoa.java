972c7b1528fe877aa3258fb8565cd3869e9dcd9e
Guilherme Zequini <guilherzb02@gmail.com>
2024-10-18 15:29:45 -0300

    atividade1

---
+++ exercicio2/Pessoa.java
@@ -0,0 +1,17 @@
+class Pessoa{
+    private String nome;
+    private String email;
+
+
+    public Pessoa (String nome , String email ){
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public PEssoa (String nome){
+        this.nome = nome ; 
+        
+    }
+
+
+}
